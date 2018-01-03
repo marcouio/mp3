dd45cd431250fc57d5b41db964f7b125b172c2f2
Marco Molinari <marco.molinari1980@gmail.com>
2018-01-03 23:31:17 +0100

    SONAR

--- src/main/java/com/molinari/mp3/business/Mp3ReaderUtil.java
+++ src/main/java/com/molinari/mp3/business/Mp3ReaderUtil.java
@@ -5,8 +5,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
 
 import javax.swing.ImageIcon;
 import javax.xml.parsers.DocumentBuilder;
@@ -16,11 +17,17 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
+import com.molinari.utility.controller.ControlloreBase;
+
 import javazoom.jl.converter.Converter;
 import javazoom.jl.decoder.JavaLayerException;
 
 public class Mp3ReaderUtil {
 
+	private Mp3ReaderUtil() {
+		//do nothing
+	}
+	
 	public static void convert(String source, String target) throws JavaLayerException{
 		Converter c = new Converter();
 		c.convert(source, target);
@@ -52,23 +59,22 @@ public class Mp3ReaderUtil {
 		return new ImageIcon(newimg);
 	}
 
-	public static void scriviFileSuPiuRighe(final File file, final ArrayList<String> righe) {
-		try {
-			final BufferedWriter out = new BufferedWriter(new FileWriter(file));
+	public static void scriviFileSuPiuRighe(final File file, final List<String> righe) {
+		try (final BufferedWriter out = new BufferedWriter(new FileWriter(file));){
+			
 			for (final Iterator<String> iterator = righe.iterator(); iterator.hasNext();) {
 				final String type = iterator.next();
 				out.write(type);
 				out.newLine();
 			}
-			out.close();
 		} catch (final IOException e) {
+			ControlloreBase.getLog().log(Level.SEVERE, e.getMessage(), e);
 		}
 	}
 
 	public static Document createDocument(final File xml) throws ParserConfigurationException, SAXException, IOException {
 		final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		final Document doc = dBuilder.parse(xml);
-		return doc;
+		return dBuilder.parse(xml);
 	}
 }
